35d1ff54f5bf0d93eef84bcb840775043ae4e3be
Toan Nguyen <toannq177@gmail.com>
2023-12-05 13:16:06 -0600

    [r] ratios that passed.

--- ratios.py
+++ ratios.py
@@ -5,58 +5,48 @@ TASK: ratios
 """
 
 
-def calc_mix(mixes, ratios):
-    x = mixes[0][0] * ratios[0] + mixes[1][0] * ratios[1] + mixes[2][0] * ratios[2]
-    y = mixes[0][1] * ratios[0] + mixes[1][1] * ratios[1] + mixes[2][1] * ratios[2]
-    z = mixes[0][2] * ratios[0] + mixes[1][2] * ratios[1] + mixes[2][2] * ratios[2]
-    return (x, y, z)
-
-
-def calc_valid_goals(mixes, goal):
-    MAX = float("inf")
-    for i in range(3):
-        if goal[i] == 0:
-            continue
-        max_sum = mixes[0][i] * 99 + mixes[1][i] * 99 + mixes[2][i] * 99
-        MAX = min(MAX, max_sum // goal[i])
-
-    valid = {}
-    for i in range(1, MAX + 1):
-        x, y, z = goal[0] * i, goal[1] * i, goal[2] * i
-        valid[(x, y, z)] = i
-    return valid
-
-
-def find(mixes, goal):
-    if goal == (0, 0, 0):
-        return (0, 0, 0), 0
-
-    valid = calc_valid_goals(mixes, goal)
-    for i0 in range(100):
-        for i1 in range(100):
-            for i2 in range(100):
-                mix = calc_mix(mixes, (i0, i1, i2))
-                if mix in valid:
-                    return (i0, i1, i2), valid[mix]
-
-    return None, None
+def close(*fobjs):
+    for f in fobjs:
+        f.close()
 
 
 def main():
-    with open("ratios.in", "r") as f:
-        tmp = [[int(x) for x in line.strip().split()] for line in f]
-        goal = tuple(tmp[0])
-        mixes = [tuple(x) for x in tmp[1:]]
+    fin = open("ratios.in", "r")
+    fout = open("ratios.out", "w")
+    tmp = [[int(x) for x in line.strip().split()] for line in fin]
+    goal = tuple(tmp[0])
+    m0, m1, m2 = [tuple(x) for x in tmp[1:]]
 
-    ratio, multiple = find(mixes, goal)
-    with open("ratios.out", "w") as f:
-        if ratio is None:
-            f.write("NONE\n")
-        else:
-            ratio = list(ratio)
-            ratio.append(multiple)
-            ratio = " ".join([str(x) for x in ratio])
-            f.write(f"{ratio}\n")
+    if goal == (0, 0, 0):
+        fout.write("0 0 0 0\n")
+        close(fin, fout)
+        return
+
+    nonzero_idx = [i for i in range(3) if goal[i] != 0][0]
+    idx1 = (nonzero_idx + 1) % 3
+    idx2 = (nonzero_idx - 1) % 3
+    mix = [0] * 3
+
+    # the key here is to remove all overheads such as separating this code block below
+    # into another function...
+    for i0 in range(100):
+        for i1 in range(100):
+            for i2 in range(100):
+                mix[0] = m0[0] * i0 + m1[0] * i1 + m2[0] * i2
+                mix[1] = m0[1] * i0 + m1[1] * i1 + m2[1] * i2
+                mix[2] = m0[2] * i0 + m1[2] * i1 + m2[2] * i2
+                if mix[nonzero_idx] != 0 and mix[nonzero_idx] % goal[nonzero_idx] == 0:
+                    multiple = mix[nonzero_idx] // goal[nonzero_idx]
+                    if (
+                        mix[idx1] == goal[idx1] * multiple
+                        and mix[idx2] == goal[idx2] * multiple
+                    ):
+                        fout.write(f"{i0} {i1} {i2} {multiple}\n")
+                        close(fin, fout)
+                        return
+
+    fout.write("NONE\n")
+    close(fin, fout)
 
 
 if __name__ == "__main__":
