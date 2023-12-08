2179b53d8efb6623a13360751b409f8357ddda72
Toan Nguyen <toannq177@gmail.com>
2023-12-08 07:24:49 -0600

    WIP

--- camelot.py
+++ camelot.py
@@ -13,7 +13,6 @@ def main():
             data.extend(line.strip().split())
 
     R, C = int(data[0]), int(data[1])
-    MIN, MAX = 0, R * C - 1
     king_c = ord(data[2]) - ord("A")
     king_r = R - int(data[3])
     king = king_r * C + king_c
@@ -25,69 +24,85 @@ def main():
         knights.append(r * C + c)
 
     king_dirs = [
-        -C,
-        -C - 1,
-        -C + 1,
-        1,
-        C + 1,
-        C,
-        C - 1,
-        -1,
+        (-1, -1),
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
     ]
     knight_dirs = [
-        -2 * C - 1,
-        -2 * C + 1,
-        -C + 2,
-        -C - 2,
-        C + 2,
-        C - 2,
-        2 * C + 1,
-        2 * C - 1,
+        (-2, -1),
+        (-2, 1),
+        (-1, -2),
+        (-1, 2),
+        (1, -2),
+        (1, 2),
+        (2, -1),
+        (2, 1),
     ]
 
     def shortest_path(start, is_king=False):
         dirs = king_dirs if is_king else knight_dirs
-        cache = {i: float("inf") for i in range(MAX + 1)}
+        cache = {i: float("inf") for i in range(R * C)}
         maps = {}
 
         cache[start] = 0
         h = [(0, start, None)]
-        for _ in range(MAX + 1):
+        for _ in range(R * C):
             dist, node, parent = H.heappop(h)
             maps[node] = parent
-            for dir in dirs:
-                next_node = node + dir
-                if MIN <= next_node <= MAX and cache[next_node] > dist + 1:
+            r = node // C
+            c = node % C
+            for dr, dc in dirs:
+                nr = r + dr
+                nc = c + dc
+                next_node = nr * C + nc
+                if 0 <= nr < R and 0 <= nc < C and cache[next_node] > dist + 1:
                     cache[next_node] = dist + 1
                     H.heappush(h, (dist + 1, next_node, node))
 
         return cache, maps
 
     king_dist = shortest_path(king, is_king=True)[0]
-    knight_dists = []
-    knight_maps = []
-    for knight in knights:
-        dist, maps = shortest_path(knight, is_king=False)
-        knight_dists.append(dist)
-        knight_maps.append(maps)
+    all_dists = []
+    all_maps = []
+    for loc in range(R * C):
+        cache, maps = shortest_path(loc, is_king=False)
+        all_dists.append(cache)
+        all_maps.append(maps)
+
+    knight_dist = {}
+    closest_knight = {}
+    for loc in range(R * C):
+        total = 0
+        min_knight = min_dist = None
+        for knight in knights:
+            d = all_dists[knight][loc]
+            total += d
+            if min_dist is None or min_dist > d:
+                min_dist = d
+                min_knight = knight
+        closest_knight[loc] = min_knight
+        knight_dist[loc] = total
+
 
     res = float("inf")
-    for loc in range(MAX + 1):
-        dist = 0
-        for knight_dist in knight_dists:
-            dist += knight_dist[loc]
+    for loc in range(R * C):
+        total_knight = knight_dist[loc]
+        total_king = king_dist[loc]
 
-        # try to get on one of the knights
-        all_knight_locs = set()
-        for knight_map in knight_maps:
-            curr = loc
-            while curr is not None:
-                all_knight_locs.add(curr)
-                curr = knight_map[curr]
-        dist += min([king_dist[_loc] for _loc in all_knight_locs])
-        res = min(res, dist)
-        if res == 9:
-            print(loc)
+        # king go by himself
+        res = min(res, total_knight + total_king)
+
+        # king try to get a ride
+        if knights:
+            for meet_loc in range(R * C):
+                chosen_knight = closest_knight[meet_loc]
+                meet_dist = total_knight - all_dists[chosen_knight][loc] + king_dist[meet_loc] + all_dists[chosen_knight][meet_loc] + all_dists[meet_loc][loc]
+                res = min(res, meet_dist)
 
     with open("camelot.out", "w") as f:
         f.write(f"{res}\n")
