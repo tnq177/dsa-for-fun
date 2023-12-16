57fa2b65100ad90b3aae858c153cde2fb172ae4c
Toan Nguyen <toannq177@gmail.com>
2023-12-15 22:46:32 -0600

    [c] add sort

--- cses/sorting+searching/1621_distinct.py
+++ cses/sorting+searching/1621_distinct.py
@@ -6,4 +6,16 @@ def main():
         seen.add(num)
     print(len(seen))
 
-main()
+def main2():
+    _ = input()
+    nums = input().strip().split()
+    nums = [int(x) for x in nums]
+    nums.sort()
+    res = 0
+    for i in range(len(nums)):
+        if i == 0 or nums[i] != nums[i - 1]:
+            res += 1
+    print(res)
+
+# main()
+main2()
