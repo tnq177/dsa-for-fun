d0d08a064f65ff5a2dadd3a011b263616c9b5ea4
Toan Nguyen <toannq177@gmail.com>
2023-12-21 23:13:25 -0600

    [f] trie, use number not string

--- cses/string/1731_word_combinations.py
+++ cses/string/1731_word_combinations.py
@@ -1,41 +1,29 @@
 def main():
-    text = input().strip()
-    matches = [[] for _ in range(len(text))]
-
+    text = input()
+    text = [ord(c) - ord("a") for c in text]
     k = int(input())
+    trie = {}
     for _ in range(k):
-        word = input().strip()
-        if len(word) > len(text):
-            continue
-
-        pi = [0] * len(word)
-        for i in range(1, len(word)):
-            j = pi[i - 1]
-            while j and word[j] != word[i]:
-                j = pi[j - 1]
-            if word[j] == word[i]:
-                pi[i] = j + 1
-
-        j = 0
-        i = 0
-        while len(text) - i >= len(word) - j:
-            while j and word[j] != text[i]:
-                j = pi[j - 1]
-            if word[j] == text[i]:
-                j += 1
-                if j == len(word):
-                    matches[i + 1 - len(word)].append(len(word))
-                    j = pi[j - 1]
-            i += 1
+        word = input()
+        curr = trie
+        for c in word:
+            curr = curr.setdefault(ord(c) - ord("a"), {})
+        curr[-1] = True
 
     MOD = 10**9 + 7
     dp = [0] * (len(text) + 1)
-    for i in range(len(text)):
-        prev = 1 if i == 0 else dp[i]
+    dp[0] = 1
+
+    for i, prev in enumerate(dp):
         if not prev:
             continue
-        for word_len in matches[i]:
-            dp[i + word_len] = (dp[i + word_len] + prev) % MOD
+        curr = trie
+        j = i
+        while j < len(text) and text[j] in curr:
+            curr = curr[text[j]]
+            j += 1
+            if -1 in curr:
+                dp[j] = (dp[j] + prev) % MOD
 
     print(dp[-1])
 
