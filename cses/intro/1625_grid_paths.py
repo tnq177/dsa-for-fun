379e02b45f9ee6e5e9c23911ad75928b9cbfc951
Toan Nguyen <toannq177@gmail.com>
2024-01-15 11:25:47 -0500

    [r] minor, still TLE

--- cses/intro/1625_grid_paths.py
+++ cses/intro/1625_grid_paths.py
@@ -12,56 +12,55 @@ def main():
 
     GRID_SIZE = 9
     seen = [[False] * GRID_SIZE for _ in range(GRID_SIZE)]
+    seen[1][1] = True
     for i in range(GRID_SIZE):
         seen[0][i] = True
         seen[-1][i] = True
         seen[i][0] = True
         seen[i][-1] = True
 
-    seen[1][1] = True
-
-    res = [0]
 
     def check1(r, c):
         return (
+            r,
+            c - 1,
             c > 2
             and not seen[r][c - 1]
             and seen[r][c - 2]
             and (seen[r - 1][c - 1] or seen[r + 1][c - 1]),
-            r,
-            c - 1,
         )
 
     def check2(r, c):
         return (
+            r,
+            c + 1,
             c < 6
             and not seen[r][c + 1]
             and seen[r][c + 2]
             and (seen[r - 1][c + 1] or seen[r + 1][c + 1]),
-            r,
-            c + 1,
         )
 
     def check3(r, c):
         return (
+            r - 1,
+            c,
             r > 2
             and not seen[r - 1][c]
             and seen[r - 2][c]
             and (seen[r - 1][c - 1] or seen[r - 1][c + 1]),
-            r - 1,
-            c,
         )
 
     def check4(r, c):
         return (
+            r + 1,
+            c,
             r < 6
             and not seen[r + 1][c]
             and seen[r + 2][c]
             and (seen[r + 1][c - 1] or seen[r + 1][c + 1]),
-            r + 1,
-            c,
         )
 
+    res = [0]
     def search(path_id, r, c):
         if (
             seen[r + 1][c]
@@ -77,6 +76,7 @@ def main():
             and not seen[r - 1][c]
         ):
             return
+
         if r == 7 and c == 1:
             if path_id == 48:
                 res[0] += 1
@@ -89,19 +89,21 @@ def main():
                 seen[nr][nc] = True
                 search(path_id + 1, nr, nc)
                 seen[nr][nc] = False
-        else:
-            for act in (check1, check2, check3, check4):
-                yes, nr, nc = act(r, c)
-                if yes:
-                    seen[nr][nc] = True
-                    search(path_id + 1, nr, nc)
-                    seen[nr][nc] = False
-                    return
-            for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-                if not seen[nr][nc]:
-                    seen[nr][nc] = True
-                    search(path_id + 1, nr, nc)
-                    seen[nr][nc] = False
+            return
+
+        for act in (check1, check2, check3, check4):
+            nr, nc, yes = act(r, c)
+            if yes:
+                seen[nr][nc] = True
+                search(path_id + 1, nr, nc)
+                seen[nr][nc] = False
+                return
+
+        for nr, nc in ((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)):
+            if not seen[nr][nc]:
+                seen[nr][nc] = True
+                search(path_id + 1, nr, nc)
+                seen[nr][nc] = False
 
     search(0, 1, 1)
     print(res[0])
