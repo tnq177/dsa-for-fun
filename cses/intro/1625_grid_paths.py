b7a7fbaad399ea79ad400034eb188d0269449dc3
Toan Nguyen <toannq177@gmail.com>
2024-01-15 11:11:20 -0500

    [r] faster gridpath (still TLE)

--- cses/intro/1625_grid_paths.py
+++ cses/intro/1625_grid_paths.py
@@ -22,6 +22,46 @@ def main():
 
     res = [0]
 
+    def check1(r, c):
+        return (
+            c > 2
+            and not seen[r][c - 1]
+            and seen[r][c - 2]
+            and (seen[r - 1][c - 1] or seen[r + 1][c - 1]),
+            r,
+            c - 1,
+        )
+
+    def check2(r, c):
+        return (
+            c < 6
+            and not seen[r][c + 1]
+            and seen[r][c + 2]
+            and (seen[r - 1][c + 1] or seen[r + 1][c + 1]),
+            r,
+            c + 1,
+        )
+
+    def check3(r, c):
+        return (
+            r > 2
+            and not seen[r - 1][c]
+            and seen[r - 2][c]
+            and (seen[r - 1][c - 1] or seen[r - 1][c + 1]),
+            r - 1,
+            c,
+        )
+
+    def check4(r, c):
+        return (
+            r < 6
+            and not seen[r + 1][c]
+            and seen[r + 2][c]
+            and (seen[r + 1][c - 1] or seen[r + 1][c + 1]),
+            r + 1,
+            c,
+        )
+
     def search(path_id, r, c):
         if (
             seen[r + 1][c]
@@ -49,43 +89,14 @@ def main():
                 seen[nr][nc] = True
                 search(path_id + 1, nr, nc)
                 seen[nr][nc] = False
-        elif (
-            c > 2
-            and not seen[r][c - 1]
-            and seen[r][c - 2]
-            and (seen[r - 1][c - 1] or seen[r + 1][c - 1])
-        ):
-            seen[r][c - 1] = True
-            search(path_id + 1, r, c - 1)
-            seen[r][c - 1] = False
-        elif (
-            c < 6
-            and not seen[r][c + 1]
-            and seen[r][c + 2]
-            and (seen[r - 1][c + 1] or seen[r + 1][c + 1])
-        ):
-            seen[r][c + 1] = True
-            search(path_id + 1, r, c + 1)
-            seen[r][c + 1] = False
-        elif (
-            r > 2
-            and not seen[r - 1][c]
-            and seen[r - 2][c]
-            and (seen[r - 1][c - 1] or seen[r - 1][c + 1])
-        ):
-            seen[r - 1][c] = True
-            search(path_id + 1, r - 1, c)
-            seen[r - 1][c] = False
-        elif (
-            r < 6
-            and not seen[r + 1][c]
-            and seen[r + 2][c]
-            and (seen[r + 1][c - 1] or seen[r + 1][c + 1])
-        ):
-            seen[r + 1][c] = True
-            search(path_id + 1, r + 1, c)
-            seen[r + 1][c] = False
         else:
+            for act in (check1, check2, check3, check4):
+                yes, nr, nc = act(r, c)
+                if yes:
+                    seen[nr][nc] = True
+                    search(path_id + 1, nr, nc)
+                    seen[nr][nc] = False
+                    return
             for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
                 if not seen[nr][nc]:
                     seen[nr][nc] = True
